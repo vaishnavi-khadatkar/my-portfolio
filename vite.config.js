4ab4b0df1a73f753568a06b45adfd7595f4f054e
vaishnavi-khadatkar <khadatkarvaishnavi@gmail.com>
2026-01-03 14:51:05 +0530

    Fix defineConfig import in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/',   // 👈 repo नाव
+  base: '/my-portfolio/',   // repo name
 })
